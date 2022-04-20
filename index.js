e9d5a224212bc0a78814bed061a82ecb5a96ebc7
mdmehedihasan212 <mdmehedihasan384@gmail.com>
2022-04-20 21:32:43 +0600

    add data post and search quary

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello Mr! How are you? Are you ready for node journey')
 })
 
 const users = [
-    { id: 1, Name: 'masrafi', Email: 'masrafi@gmail.com' },
-    { id: 2, Name: 'tamim', Email: 'tamim@gmail.com' },
-    { id: 3, Name: 'musfiq', Email: 'musfiq@gmail.com' },
-    { id: 4, Name: 'mahmudullah', Email: 'mahmudullah@gmail.com' },
-    { id: 5, Name: 'sakib', Email: 'sakib@gmail.com' },
-    { id: 6, Name: 'imrul', Email: 'imrul@gmail.com' },
+    { id: 1, name: 'masrafi', email: 'masrafi@gmail.com' },
+    { id: 2, name: 'tamim', email: 'tamim@gmail.com' },
+    { id: 3, name: 'musfiq', email: 'musfiq@gmail.com' },
+    { id: 4, name: 'riyad', email: 'riyad@gmail.com' },
+    { id: 5, name: 'sakib', email: 'sakib@gmail.com' },
+    { id: 6, name: 'imrul', email: 'imrul@gmail.com' },
 ];
 
 app.get('/users', (req, res) => {
-    res.send(users);
+    if (req.query.name) {
+        const search = req.query.name.toLowerCase();
+        const matched = users.filter(user => user.name.toLowerCase().includes(search))
+        res.send(matched);
+        console.log('query', req.query);
+    }
+    else {
+        res.send(users);
+    }
+})
+
+app.post('/user', (req, res) => {
+    const user = req.body;
+    user.id = users.length + 1;
+    users.push(user);
+    console.log('request', req.body);
+    res.send(user);
 })
 
 // users search with params
